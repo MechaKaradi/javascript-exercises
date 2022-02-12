47c9cf55dbe48198740388d71f48930912cbcbbd
MechaKaradi <61778659+MechaKaradi@users.noreply.github.com>
2022-02-12 22:37:36 +0530

    ex 1 and 2 [Done]

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if (number <0 ) {
+        return 'ERROR'      
+    }
+    let output = ""
+    for (i = 0; i < number; i++) {
+        output = output + string;
+    }
+    return output;
 };
 
 // Do not edit below this line
