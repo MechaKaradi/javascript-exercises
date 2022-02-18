e49d928df26e4381bae124c4e940f0705bcff4c3
MechaKaradi <61778659+MechaKaradi@users.noreply.github.com>
2022-02-18 22:23:44 +0530

    Completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    
+    //Check for whether it is a number. Check for if it is greater than 0
+    if (!(typeof(a) == 'number')) { return "ERROR";}
+    if (a<0) {return "ERROR";}
+    if (!(typeof(b) == 'number')) { return "ERROR";}
+    if (b<0) {return "ERROR";}
 
+    //Order correctly for low and high
+
+    let A = Math.min(a,b);
+    let B = Math.max(a,b);
+
+    // Make sum 
+    
+    let sum = 0;
+    for (i=A; i<=B; i++) {
+        sum+=i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
