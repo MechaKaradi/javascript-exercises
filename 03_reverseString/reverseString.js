eb37949751e0ef1bffd94d7fee2054aba7624a76
MechaKaradi <61778659+MechaKaradi@users.noreply.github.com>
2022-02-18 21:40:12 +0530

    Solved removeFromArray

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let text = string;
+    let count = text.length;
+    let reversedString = "";
+    while (count > 0) {
+        count--
+        reversedString = reversedString + text[count];        
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayInput,) {
+    //import array and check that it is an array
+    array = Array.from(arrayInput);
+    if (!Array.isArray(array)) { return "Error. not an array"} 
+    
+    //Turn
+     for (i=1; i < arguments.length; i++) {
+        
+        if (!array.includes(arguments[i])) {continue;}
+        removeIndex = array.indexOf(arguments[i]);
+        array.splice(removeIndex,1);
+     }
+
+    return array;
 
 };
 
