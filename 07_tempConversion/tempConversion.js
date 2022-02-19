fb61544e0d524d97bf14a2a7d8dea2828c7e3b83
MechaKaradi <61778659+MechaKaradi@users.noreply.github.com>
2022-02-19 17:58:54 +0530

    07 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
+const ftoc = function(number) {
+  if (isNaN(number)) {return "ERROR"}
+
+  temprature = (number -32)*5/9;
+
+  return Math.round(temprature*10)/10;
 
 };
 
-const ctof = function() {
+const ctof = function(number) {
+  if (isNaN(number)) {return "ERROR"}
+  temprature = (number*9/5)+32;
 
+  return Math.round(temprature*10)/10;
+ 
 };
 
 // Do not edit below this line
