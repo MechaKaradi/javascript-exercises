db3e556a17f99c8d55379ff487bf709baa84e67e
MechaKaradi <61778659+MechaKaradi@users.noreply.github.com>
2022-02-19 10:27:56 +0530

    completed 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //return false;
+    if (isNaN(year)) {return "ERROR";}
 
-};
+    if (year%4 != 0) {return false;} //continues if true  
+    
+    if (year%100 == 0) {
+        if (year%400 == 0) {
+            return true;
+        } else {return false;}
+    } else {return true;}
+}
 
 // Do not edit below this line
 module.exports = leapYears;
